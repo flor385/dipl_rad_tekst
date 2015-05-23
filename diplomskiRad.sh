218356244b1c4cafa61214e8d092eb2b9853b08e
Florijan Stamenković <florijan.stamenkovic@styria.hr>
2015-05-23 12:45:54 +0200

    Work on tex.

--- diplomskiRad.sh
+++ diplomskiRad.sh
@@ -7,7 +7,7 @@ pdflatex --shell-escape diplomskiRad.tex
 bibtex diplomskiRad.aux
 pdflatex --shell-escape diplomskiRad.tex
 pdflatex --shell-escape diplomskiRad.tex
-open diplomskiRad.pdf
+open diplomskiRad.pdf || xdg-open diplomskiRad.pdf
 
 #	brisemo sav meta shit
 rm *.dv *.bbl *.aux *blg *.dvi *.log *.gz *.toc
